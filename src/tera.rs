306b02ad62e96c952be09ab173d17a680fe2b0f6
Sanpi <sanpi@homecomputing.fr>
2026-06-26 15:17:44 +0200

    Updates tera to 2.0

--- src/tera.rs
+++ src/tera.rs
@@ -1,47 +1,44 @@
 pub struct Pager;
 
-macro_rules! get_arg {
-    ($args:ident, $name:literal, $ty:ty) => {
-        match $args.get($name) {
-            Some(value) => tera::from_value::<$ty>(value.clone())?,
-            None => {
-                return Err(tera::Error::msg(format!(
-                    "Function `{}` require pager argument",
-                    $name
-                )))
-            }
-        }
-    };
-
-    ($args:ident, $name:literal, $ty:ty, $default:expr) => {
-        match $args.get($name) {
-            Some(value) => tera::from_value::<$ty>(value.clone())?,
-            None => $default,
-        }
-    };
+#[derive(serde::Deserialize)]
+struct Param {
+    pager: crate::Pager,
+    #[serde(default)]
+    base_url: String,
+    #[serde(default = "default_page_param")]
+    page_param: String,
+    #[serde(default = "default_limit_param")]
+    limit_param: String,
+    #[serde(default = "default_ellipsis_param")]
+    ellipsis: usize,
 }
 
-impl tera::Function for Pager {
-    fn call(
-        &self,
-        args: &std::collections::HashMap<String, tera::Value>,
-    ) -> tera::Result<tera::Value> {
-        let pager = get_arg!(args, "pager", crate::Pager);
-        let base_url = get_arg!(args, "base_url", String, String::new());
-        let page_param = get_arg!(args, "page_param", String, "page".to_string());
-        let limit_param = get_arg!(args, "limit_param", String, "limit".to_string());
-        let ellipsis = get_arg!(args, "ellipsis", usize, 9);
+fn default_page_param() -> String {
+    "page".to_string()
+}
+
+fn default_limit_param() -> String {
+    "limit".to_string()
+}
+
+fn default_ellipsis_param() -> usize {
+    9
+}
+
+impl tera::Function<tera::TeraResult<String>> for Pager {
+    fn call(&self, kwargs: tera::Kwargs, _: &tera::State<'_>) -> tera::TeraResult<String> {
+        let param = kwargs.deserialize::<Param>()?;
 
         let config = crate::pager::Config {
-            base_url,
-            page_param,
-            limit_param,
-            ellipsis,
+            base_url: param.base_url,
+            page_param: param.page_param,
+            limit_param: param.limit_param,
+            ellipsis: param.ellipsis,
         };
 
-        let html = crate::html::pager(&pager, &config);
+        let html = crate::html::pager(&param.pager, &config);
 
-        Ok(html.into())
+        Ok(html)
     }
 
     fn is_safe(&self) -> bool {
